5356d901583f1cdf1ab625394ab1b82c65f97828
user <hojiakbar_1997@mail.ru>
2022-01-30 00:41:18 +0600

    будильник

--- app/src/main/java/com/example/play/MainActivity2.kt
+++ app/src/main/java/com/example/play/MainActivity2.kt
@@ -1,13 +1,10 @@
 package com.example.play
 
 import android.media.MediaPlayer
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.util.Log
 import android.widget.*
-import androidx.annotation.RequiresApi
 
 class MainActivity2 : AppCompatActivity() {
 
@@ -16,10 +13,15 @@ class MainActivity2 : AppCompatActivity() {
     var edit: EditText? = null
     var image: ImageView? = null
     var text: TextView? = null
+    var time: TextView? = null
 
     private var timer: CountDownTimer? = null
 
     var abc: Long? = null
+
+    var starPoint = 0
+
+    var endPoint = 0
 
     var mp: MediaPlayer? = null
 
@@ -33,15 +35,45 @@ class MainActivity2 : AppCompatActivity() {
         image = findViewById(R.id.lampochka)
 
         text = findViewById(R.id.textView2)
+        time = findViewById(R.id.minutes)
 
-
+        abc = 4
         start?.setOnClickListener {
-            abc = edit?.text.toString() as Long
+//            abc = edit?.text.toString().toLong()
 //            seekBar?.max = edit?.text.toString().toInt()
             startCountDownTimer(abc!!)
         }
 
 
+        seekBar?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, progress: Int, p2: Boolean) {
+                time?.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+//                if (seekBar != null) {
+//                    starPoint = seekBar.progress
+//
+//                }
+//                startCountDownTimer(starPoint!!.toLong())
+                //                startCountDownTimer(text.toString().toLong()!!)
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                if (seekBar != null) {
+                    endPoint = seekBar.progress
+
+                }
+                startCountDownTimer(endPoint!!.toLong())
+
+//                seekBar?.apply {
+//                    progress = endPoint
+//                }
+
+                //                startCountDownTimer(text.toString().toLong()!!)
+            }
+
+        })
     }
 
     private fun startCountDownTimer(abc: Long) {
@@ -54,13 +86,14 @@ class MainActivity2 : AppCompatActivity() {
         timer = object : CountDownTimer(abc * 60000, 1000) {
             override fun onTick(timeM: Long) {
                 image!!.setImageResource(R.drawable.bulb_off)
-                text!!.text = ("" + timeM / 1000)
-                val result = (timeM / 1000).toInt()
-                seekBar?.apply {
-                    progress = result
-
-                }
-
+                time!!.text = ("" + timeM / 60000)
+                text!!.text = (":" + timeM / 1000)  ////*/*/*/*/*/секунды можно ускорить нолик добавив и на экране тоже + цифра
+                val result = (timeM / 60000).toInt()
+//                seekBar?.apply {
+//                    progress = result
+//
+//                }
+                seekBar!!.progress = result
 //
             }
 
